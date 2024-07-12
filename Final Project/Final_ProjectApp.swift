aeceac53afc7069a4f5a00092469064eb4a19e9f
Luna <gonzalezgonzalezl@catlin.edu>
2024-07-12 13:02:30 -0700

    Initial Commit

---
+++ Final Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final Project
+//
+//  Created by Luna Gonzalez on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
